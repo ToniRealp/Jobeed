fc13ce6e01e965243984aad5591da7f0be2fcb3b
ToniRealp <tonirealp@gmail.com>
2021-12-13 13:39:47 +0100

    Added config for heroku deployment

--- src/main/java/platform/shared/infrastructure/MySqlConnection.java
+++ src/main/java/platform/shared/infrastructure/MySqlConnection.java
@@ -1,16 +1,19 @@
 package platform.shared.infrastructure;
 
 import java.sql.*;
+import java.util.Map;
 
 public class MySqlConnection {
 
     private final Connection connection;
 
     public MySqlConnection() throws Exception {
-        String user = "root";
-        String password = "password";
+        Map<String, String> env = System.getenv();
+        String user = env.get("MYSQL_USER");
+        String password = env.get("MYSQL_PWD");
+        String dbURL = env.get("MYSQL_URL");
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://mysql/jobeed?user=" + user + "&password=" + password);
+        connection = DriverManager.getConnection("jdbc:" + dbURL + "/jobeed?user=" + user + "&password=" + password);
     }
 
     public PreparedStatement prepareStatement(String query) throws SQLException {
